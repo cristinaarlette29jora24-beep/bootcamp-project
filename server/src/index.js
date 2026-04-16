c2d899bcde6fbf102d48a9e3909e0e30eb7d2e71
cristinaarlette29jora24-beep <cristinaarlette29jora24@gmail.com>
2026-04-16 10:41:59 +0200

    feat: instalar y configurar cors y middleware json

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // 1. Importamos el portero que acabas de instalar
 const taskRoutes = require('./routes/task.routes');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+// --- CONFIGURACIÓN DE MIDDLEWARES ---
+
+app.use(cors());           // 2. Encendemos el portero (permite que la web hable con la API)
+app.use(express.json());   // 3. Encendemos el traductor (permite leer el título de la tarea)
+
+// --- RUTAS ---
 
 app.use('/api/v1/tasks', taskRoutes);
 
-app.listen(3000, () => console.log('🚀 Servidor en puerto 3000'));
+app.get('/', (req, res) => {
+    res.send('Servidor TaskFlow conectado y funcionando 🚀');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`);
+});
